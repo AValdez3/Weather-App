6ba2c7f55b80d0ec479ab28430ce914e9280a5a4
Avelardo Valdez <avelardoval@gmail.com>
2020-08-04 11:54:33 -0700

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Avelardo Valdez on 8/4/20.
+//  Copyright © 2020 Avelardo Valdez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
