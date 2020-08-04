8c709f37035b198667090f5af4b579e62b6b345f
Avelardo Valdez <avelardoval@gmail.com>
2020-08-04 15:05:03 -0700

    Layout the UI

--- Weather App/ChooseCityViewController.swift
+++ Weather App/ChooseCityViewController.swift
@@ -6,4 +6,48 @@
 //  Copyright © 2020 Avelardo Valdez. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol ChooseCityViewControllerDelegate: class {
+    func chosenCity(city: String)
+}
+
+class ChooseCityViewController: UIViewController {
+    
+    private let cityNameInput = UITextField()
+    private let chooseCityButton = UIButton(type: .system)
+    
+    weak var delegate: ChooseCityViewControllerDelegate?
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        setUpTextInputField()
+        setUpChooseCityButton()
+    }
+    
+    
+    private func setUpTextInputField() {
+        cityNameInput.placeholder = "Enter a City Name"
+        cityNameInput.textAlignment = .center
+        view.addSubview(cityNameInput)
+        cityNameInput.translatesAutoresizingMaskIntoConstraints = false
+        cityNameInput.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
+        cityNameInput.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.70).isActive = true
+        cityNameInput.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    private func setUpChooseCityButton() {
+        chooseCityButton.setTitle("Get Weather", for: .normal)
+        chooseCityButton.addTarget(self, action: #selector(getWeather), for: .touchUpInside)
+        view.addSubview(chooseCityButton)
+        chooseCityButton.translatesAutoresizingMaskIntoConstraints = false
+        chooseCityButton.topAnchor.constraint(equalTo: cityNameInput.bottomAnchor, constant: 10).isActive = true
+        chooseCityButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    @objc private func getWeather() {
+        print(cityNameInput.text)
+    }
+    
+}
