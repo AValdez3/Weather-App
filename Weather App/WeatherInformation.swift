3a563ab9829c9653ea05218f91d73dee694ace0b
Avelardo Valdez <avelardoval@gmail.com>
2020-08-04 15:01:47 -0700

    Rename ViewController to WeatherViewController

---
+++ Weather App/WeatherInformation.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherInformation.swift
+//  Weather App
+//
+//  Created by Avelardo Valdez on 8/4/20.
+//  Copyright © 2020 Avelardo Valdez. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherInformation {
+    
+    var cityName = "Atwater"
+    var temperature = "40"
+    var condition = "Sunny"
+    
+    
+}
--- Weather App/WeatherViewController.swift
+++ Weather App/WeatherViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
