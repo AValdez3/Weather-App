0f0184b8328edcbcd8d236daa029be3bc0f00972
Avelardo Valdez <avelardoval@gmail.com>
2020-08-04 12:07:32 -0700

    Remove the storyboard and allow for a programmatic UI

--- Weather App/AppDelegate.swift
+++ Weather App/AppDelegate.swift
@@ -10,11 +10,21 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if #available(iOS 13, *) {
+            
+        } else {
+            window = UIWindow()
+            setUpRootViewController()
+        }
+        
+        
         return true
     }
 
@@ -32,6 +42,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    func setUpRootViewController() {
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+    
+    
 }
 
