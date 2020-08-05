3089404379dea4d4fbd8f0a6b844209c74aeebfc
Avelardo Valdez <avelardoval@gmail.com>
2020-08-05 12:06:23 -0700

    Create an extension to load images from a URL

--- Weather App/UIImageView+LoadImage.swift
+++ Weather App/UIImageView+LoadImage.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2020 Avelardo Valdez. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+extension UIImageView {
+    func loadImage(from url: URL) {
+        DispatchQueue.global().async {
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self.image = image
+                    }
+                }
+            }
+        }
+    }
+}
