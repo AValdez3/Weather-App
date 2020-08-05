0d116103ff3998e7143c0bd2d95a284cef068b55
Avelardo Valdez <avelardoval@gmail.com>
2020-08-05 12:05:34 -0700

    Create the JSONStructs and move the appropriate files into it

---
+++ Weather App/UIImageView+LoadImage.swift
@@ -0,0 +1,9 @@
+//
+//  UIImageView+LoadImage.swift
+//  Weather App
+//
+//  Created by Avelardo Valdez on 8/5/20.
+//  Copyright © 2020 Avelardo Valdez. All rights reserved.
+//
+
+import Foundation
